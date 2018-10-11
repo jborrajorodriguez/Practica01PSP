a213de1f1e72b404b035fe783bfb185f61a54a55
Juan Borrajo <juanborrajo10@gmail.com>
2018-10-11 10:03:38 +0200

    Inicio proyecto

---
+++ src/practica01/Practica01.java
@@ -0,0 +1,15 @@
+
+package practica01;
+
+/**
+ *
+ * @author Juan Borrajo Rodriguez
+ */
+public class Practica01 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
